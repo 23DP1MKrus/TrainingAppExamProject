27ff2818d1b5650d89828e3942b22b99b8e68db6
m1rkk <mrakkus15@gmail.com>
2025-03-12 09:29:42 +0200

    initial commit

---
+++ src/main/java/com/example/traininapp/TraininAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.traininapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraininAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraininAppApplication.class, args);
+    }
+
+}
