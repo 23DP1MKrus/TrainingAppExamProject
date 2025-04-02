64d588ff7a8f3670d5799ab6f2fdb4d2de353e5b
lizaremizova <er115@inbox.lv>
2025-04-02 23:18:29 +0300

    added LoginView class

--- src/main/java/com/example/traininapp/Views/LoginView.java
+++ src/main/java/com/example/traininapp/Views/LoginView.java
@@ -1,7 +1,59 @@
 package com.example.traininapp.Views;
 
+import com.example.traininapp.UserPack.User;
+import com.example.traininapp.UserPack.UserService;
+import com.example.traininapp.WorkoutPack.Workout;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Route("login")
-public class LoginView {
-}
+public class LoginView extends VerticalLayout {
+    private final UserService userService;
+
+    @Autowired
+    public LoginView(UserService userService) {
+        this.userService = userService;
+
+        setClassName("login-view");
+
+        VerticalLayout formLayout = new VerticalLayout();
+        formLayout.setClassName("login-form");
+
+        H1 title = new H1("LOGIN");
+        title.setClassName("login-title");
+
+        EmailField email = new EmailField("email");
+        PasswordField password = new PasswordField("password");
+
+        email.setClassName("input-email");
+        password.setClassName("input-password");
+
+        Text error = new Text("");
+        Button loginButton = new Button("Log in");
+        loginButton.setClassName("login-button");
+        loginButton.addClickListener(e -> {
+            if (userService.canLogin(email.getValue())) {
+                UI.getCurrent().navigate("main");
+            }
+            else {
+                error.setText("login-error");
+            }
+        });
+
+        formLayout.add(email, password,error, loginButton);
+        add(formLayout);
+    }
+    }
+
+
+
