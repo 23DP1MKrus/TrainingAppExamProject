f69ad743e6d4415fb6b70bc11b036c3ea2155bea
lizaremizova <er115@inbox.lv>
2025-04-06 19:12:15 +0300

    added done exercise service

---
+++ src/main/java/com/example/traininapp/DoneExPack/DoneExService.java
@@ -0,0 +1,22 @@
+package com.example.traininapp.DoneExPack;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoneExService {
+    private DoneExerciseRepo doneExerciseRepo;
+    @Autowired
+    public DoneExService(DoneExerciseRepo doneExerciseRepo) {
+        this.doneExerciseRepo = doneExerciseRepo;
+    }
+    public List<DoneExercise> getAllDoneExercises() {
+            return doneExerciseRepo.findAll();
+    }
+    public void addDoneExercise(DoneExercise doneExercise) {
+        doneExerciseRepo.save(doneExercise);
+    }
+
+}
