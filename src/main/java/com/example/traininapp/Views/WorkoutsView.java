c4b6b252de9e9f7771574a92b90e7a86225f218d
lizaremizova <er115@inbox.lv>
2025-04-22 21:22:42 +0300

    tryng to create workout view

---
+++ src/main/java/com/example/traininapp/Views/WorkoutsView.java
@@ -0,0 +1,106 @@
+package com.example.traininapp.Views;
+
+import com.example.traininapp.PlanPack.PlanService;
+import com.example.traininapp.UserPack.User;
+import com.example.traininapp.UserPack.UserService;
+import com.example.traininapp.WorkoutPack.Workout;
+import com.example.traininapp.WorkoutPack.WorkoutService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route("workouts")
+public class WorkoutsView extends Div{
+    private UserService userService;
+    private WorkoutService workoutService;
+
+    @Autowired
+    public WorkoutsView(UserService userService, WorkoutService workoutService) {
+        this.userService = userService;
+        this.workoutService = workoutService;
+
+
+        VaadinSession session = VaadinSession.getCurrent();
+        String sessionEmail = session.getAttribute("email").toString();
+        User user = userService.findByEmail(sessionEmail).orElseThrow(() -> new IllegalStateException("User not found"));
+
+        Div workoutsDiv = new Div();
+        workoutsDiv.setId("workouts-div");
+
+        VerticalLayout leftContainer = new VerticalLayout();
+        leftContainer.setId("left-container");
+
+        Image logo = new Image();
+        logo.setClassName("logo");
+
+        leftContainer.add(
+                createNavLink("PLANS", "plans"),
+                createNavLink("Exercises", "exercises"),
+                createNavLink("MAIN", "main"),
+                createNavLink("WORKOUTS", "workouts")
+        );
+
+        HorizontalLayout topContainer = new HorizontalLayout();
+        topContainer.setId("top-container");
+
+        H1 title = new H1("VIEW YOUR WORKOUTS");
+        title.setClassName("title");
+
+        Image searchIcon = new Image();
+        Button searchBtn = new Button(searchIcon);
+        searchBtn.setClassName("topBtn");
+
+        Image ringIcon = new Image();
+        Button ringBtn = new Button(ringIcon);
+        ringBtn.setClassName("topBtn");
+
+        Image profileIcon = new Image();
+        Button profileBtn = new Button(profileIcon);
+        profileBtn.setClassName("topBtn");
+
+        topContainer.add(title, searchBtn, ringBtn, profileBtn);
+
+        VerticalLayout workoutsContainer = new VerticalLayout();
+        workoutsContainer.setId("workouts-container");
+
+        for(Workout workout :user.getWorkouts()){
+            HorizontalLayout workoutsField = new HorizontalLayout();
+            workoutsField.setClassName("workout-field");
+            Paragraph workoutDate = new Paragraph(String.valueOf(workout.getDate()));
+            workoutDate.setClassName("workout-date");
+            Paragraph  timeSpentHolder = new Paragraph("time-spent:");
+            timeSpentHolder.setClassName("workout-time-spent-holder");
+            Paragraph timeSpent = new Paragraph(String.valueOf(workout.getTimeSpent()));
+            timeSpent.setClassName("workout-time-spent");
+            Paragraph  burntCalHolder = new Paragraph("burnt-cals:");
+            burntCalHolder.setClassName("workout-burnt-cal-holder");
+            Paragraph burntCals = new Paragraph(String.valueOf(workout.getBurntKcal()));
+            burntCals.setClassName("workout-burnt-cals");
+            workoutsField.add(workoutDate,timeSpentHolder,timeSpent,burntCalHolder,burntCals);
+            workoutsContainer.add(workoutsField);
+        }
+
+        workoutsDiv.add(leftContainer, topContainer, workoutsContainer);
+        add(workoutsDiv);
+    }
+
+
+    private HorizontalLayout createNavLink(String text, String route) {
+        Paragraph anchorText = new Paragraph(text);
+        Anchor anchor = new Anchor(route, "");
+        anchor.setClassName("nav-link");
+        anchorText.setClassName("nav-link-text");
+        anchorText.add(anchor);
+        HorizontalLayout navLayout = new HorizontalLayout();
+        navLayout.add(anchorText);
+        return navLayout;
+    }
+
+
+
+
+    }
